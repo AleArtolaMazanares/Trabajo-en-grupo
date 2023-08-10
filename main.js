643ea4148bcfa16c09f6c2185580653cd3df1b03
Alejandro Artola <aartolam@fwdcostarica.com>
2023-08-10 13:38:55 -0600

    terminado

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
-const lista = [1, 2, 3, 4];
-
 const sumar = (acc, value) => acc + value;
-
-const resultado = lista.reduce(sumar);
-console.log(resultado);
+console.log([1, 2, 3, 4].reduce(sumar));
